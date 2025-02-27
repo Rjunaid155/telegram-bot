1ba32a00f0086de074c08bdb5a0b144125bd61ba
Rjunaid155 <rjunaid1555@gmail.com>
2025-02-27 14:14:43 -0800

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import telebot  
 import os  
 
-# Bot Token ko environment variable se lene ka tareeqa  
-TOKEN = os.getenv("8094066627:AAFvUUyhSivGyp6yphOkhTbj6gjJGzFS73U")  
+# Token ko environment variable se sahi tareeke se lena
+TOKEN = os.getenv(8094066627:AAFvUUyhSivGyp6yphOkhTbj6gjJGzFS73U)
+
+# Token check karne ke liye
+if not TOKEN:
+    print("Error: Token not found. Make sure you set the TOKEN environment variable.")
+    exit(1)
 
 bot = telebot.TeleBot(TOKEN)
 
