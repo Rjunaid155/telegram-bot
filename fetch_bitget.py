4a5b7b988d87dc423c610b0c7504c99de0a8165b
Rjunaid155 <rjunaid1555@gmail.com>
2025-03-15 13:24:27 -0700

    fetch_bitget.py

--- fetch_bitget.py
+++ fetch_bitget.py
@@ -77,7 +77,7 @@ def main():
 
     # Get Mempool Data
     mempool_data = get_mempool_data()
-   mempool_size = mempool_data.get("vsize", None)  # "size" ki jagah "vsize"
+    mempool_size = mempool_data.get("vsize", None)  # "size" ki jagah "vsize"
 
     # Get Reddit Sentiment Score
     reddit_sentiment = analyze_reddit_sentiment("cryptocurrency", "Bitcoin")
