65383d3c47d71f2508e1a3110b9e1724973797c1
Rjunaid155 <rjunaid1555@gmail.com>
2025-04-09 14:49:34 -0700

    fetch_pine_alert.py

--- fetch_pine_alert.py
+++ fetch_pine_alert.py
@@ -4,7 +4,7 @@ import requests
 def send_telegram_message(message):
     bot_token = os.getenv("TELEGRAM_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {
         "chat_id": chat_id,
         "text": message
