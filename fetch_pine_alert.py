dfa50accb25b2d442823a5c6325f9487134b974a
Rjunaid155 <rjunaid1555@gmail.com>
2025-04-08 17:24:24 -0700

    fetch_pine_alert.py

--- fetch_pine_alert.py
+++ fetch_pine_alert.py
@@ -4,7 +4,7 @@ Telegram_token = "TOKEN"  # ← Fix yahan, pehle ye Telegram_token tha
 chat_id = "YOUR_CHAT_ID"
 message = "Test alert from MEXC bot — working perfectly!"
 
-url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+url = f"https://api.telegram.org/bot{Telegram_token}/sendMessage"
 data = {
     "chat_id": chat_id,
     "text": message
